e707c5df8145493e745e0e86cff84321118d95a5
aliasav <saurabhav.torres@gmail.com>
2016-08-23 19:19:23 +0530

    working on initialiser

---
+++ main.py
@@ -0,0 +1,14 @@
+# maintain smoking habits via terminal
+
+from smokeager import Smokeager
+
+def main():
+	pass
+
+def console_entry():
+	""" entry point for console scripts """
+	main()
+	
+if __name__=="__main__":
+	main()
+
